e70530d7df102f7144e80f0b260bd5541b000588
Isaac Ong <onglki48@gmail.com>
2026-08-26 20:54:37 +0800

    Level-0

---
+++ src/main/java/Socrates.java
@@ -0,0 +1,14 @@
+public class Socrates {
+    public static void main(String[] args) {
+        String banner = "   _____                                          \n" +
+                "  / ____|                                         \n" +
+                " | (___   ___   ___ _ __ __ _| |_ ___  ___\n" +
+                "  \\___ \\ / _ \\ / __| '__/ _` | __/ _ \\/ __|\n" +
+                "  ____) | (_) | (__| | | (_| | ||  __/\\__ \\\n" +
+                "  |____/ \\___/ \\___|_|  \\__,_|\\__\\___||___/\n" +
+                "Hello! I'm Socrates.\n" +
+                "What can I do for you? \n\n" +
+                "Bye. Hope to see you again soon!";
+        System.out.println(banner);
+    }
+}
