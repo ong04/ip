6c363528d324acce4dfbc37a4df05ee45bdf97d1
Isaac Ong <onglki48@gmail.com>
2026-08-27 12:47:37 +0800

    Echo

--- src/main/java/Socrates.java
+++ src/main/java/Socrates.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Socrates {
     public static void main(String[] args) {
         String banner = "   _____                                          \n" +
@@ -7,8 +9,26 @@ public class Socrates {
                 "  ____) | (_) | (__| | | (_| | ||  __/\\__ \\\n" +
                 "  |____/ \\___/ \\___|_|  \\__,_|\\__\\___||___/\n" +
                 "Hello! I'm Socrates.\n" +
-                "What can I do for you? \n\n" +
-                "Bye. Hope to see you again soon!";
-        System.out.println(banner);
+                "What can I do for you? \n";
+        String lineBreak = "____________________________________________________________\n";
+        String byeMessage = "\tBye. Hope to see you again soon!\n";
+        System.out.println(banner + lineBreak);
+        lineBreak = "\t" + lineBreak;
+        Scanner scanner = new Scanner(System.in);
+        while(true){
+            // get input
+            // strip input
+            String input = scanner.nextLine().strip();
+            // check if input is not bye
+            if (input.equals("bye")){
+                System.out.print(lineBreak + byeMessage + lineBreak);
+                break;
+            }
+            // print input
+            else{
+                input = "\t" + input + "\n";
+                System.out.print(lineBreak + input + lineBreak);
+            }
+        }
     }
 }
